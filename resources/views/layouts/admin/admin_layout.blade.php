ab3e4c6386c0c728924993cb03d853c459a83e90
Betty <bettypuramana@gmail.com>
2025-05-22 13:21:33 +0530

    changes

--- resources/views/layouts/admin/admin_layout.blade.php
+++ resources/views/layouts/admin/admin_layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Tubo</title>
+     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/user/images/logos/Tubo logo.png') }}">
     <!-- base:css -->
     <link rel="stylesheet" href="{{ asset('assets/admin/vendors/typicons.font/font/typicons.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/admin/vendors/css/vendor.bundle.base.css') }}">
